4d9ffb6085aed7c0752a13be6051afb896b586fd
gerrizzi <32522613+gerrizzi@users.noreply.github.com>
2021-04-11 17:44:57 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,76 +1,151 @@
 var juego = {
-    modo: 3,
+    modo: 8,
     jugadores: {
         1: {
+            nombre: "Nosotros",
             puntos: 0,
-            malas: true
+            malas: () => {
+                return juego.jugadores["1"].puntos <= (GetPuntosXModo(modo) / 2);
+            },
+            gano: () => {
+                return juego.jugadores["1"].puntos >= GetPuntosXModo(modo);
+            }
         },
         2: {
+            nombre: "Ellos",
             puntos: 0,
-            malas: true
+            malas: () => {
+                return juego.jugadores["2"].puntos <= (GetPuntosXModo(modo) / 2);
+            },
+            gano: () => {
+                return juego.jugadores["2"].puntos >= GetPuntosXModo(modo);
+            }
         }
     },
-    puntos_x_modo: { 1: 16, 2: 20, 3: 30, 4: 40 }
+    puntos_x_modo: { 1: 9, 2: 10, 3: 12, 4: 15, 5: 18, 6: 20, 7: 24, 8: 30, 9: 40 }
 }
 
 $(document).ready(function () {
     $(".tablero").click(function () {
         let _this = $(this);
         let nro_jugador = _this.attr("jugador");
-        let cant_puntos = _this.find(".cant-puntos");
-        let suma_puntos = parseInt(cant_puntos.text()) + 1;
-        let html_punto = `<div class="punto #NOMBRE_PUNTO#"></div>`;
 
-        let puntos = _this.find(".punto");
-        let ultimo_punto = puntos.length == 0 ? null : $(puntos[puntos.length - 1]);
-        let ultimo_punto_nombre = ultimo_punto == null ? "" : ultimo_punto[0].className.match(/punto-[^\s]*/)[0];
-
-        if (suma_puntos <= juego.puntos_x_modo[juego.modo + ""]) {
-            SetCantPuntos(nro_jugador, suma_puntos);
+        if (!CheckearSiGanoJugador(nro_jugador)) {
+            AddPunto(nro_jugador);
         }
-
-        if (ultimo_punto_nombre == "punto-arriba")
-            ultimo_punto.parent().append(html_punto.replace("#NOMBRE_PUNTO#", "punto-derecha"));
-
-        else if (ultimo_punto_nombre == "punto-derecha")
-            ultimo_punto.parent().append(html_punto.replace("#NOMBRE_PUNTO#", "punto-abajo"));
-
-        else if (ultimo_punto_nombre == "punto-abajo")
-            ultimo_punto.parent().append(html_punto.replace("#NOMBRE_PUNTO#", "punto-izquierda"));
-
-        else if (juego.modo != 1 && ultimo_punto_nombre == "punto-izquierda")
-            ultimo_punto.parent().append(html_punto.replace("#NOMBRE_PUNTO#", "punto-medio"));
-
-        else if ((juego.modo == 1 && ultimo_punto_nombre == "punto-izquierda") || (ultimo_punto_nombre == "punto-medio"))
-            if (suma_puntos == ((juego.puntos_x_modo[juego.modo + ""] / 2) + 1)) {
-                juego.jugadores[nro_jugador + ""].malas = false;
-                _this.find(".punto").remove();
-                _this.find(".divisor-puntos:first").append(html_punto.replace("#NOMBRE_PUNTO#", "punto-arriba"));
-            }
-            else if(suma_puntos >= juego.puntos_x_modo[juego.modo + ""] - 1){
-                $("#conf-panel").fadeIn(100);
-            }
-            else {
-                ultimo_punto.parent().next(".divisor-puntos").append(html_punto.replace("#NOMBRE_PUNTO#", "punto-arriba"));
-            }
-        else
-            _this.find(".divisor-puntos:first").append(html_punto.replace("#NOMBRE_PUNTO#", "punto-arriba"));
+        else {
+            $("#conf-panel").fadeIn(100);
+        }
     });
 
     console.log("Funcionando...");
 });
 
+function AddPuntos(nro_jugador, puntos) {
+    for (let i = 0; i < puntos; i++)
+        AddPunto(nro_jugador);
+}
+
+function AddPunto(nro_jugador) {
+    let jugador = GetDataJugador(nro_jugador);
+
+    IncrementarPuntosJugador(nro_jugador);
+    SetCantTableroPuntos(nro_jugador, jugador.puntos);
+    RemoveTableroPuntosSiPasoABuenas(nro_jugador);
+    AddTableroPunto(nro_jugador);
+}
+
+function AddTableroPunto(nro_jugador) {
+    let tablero_jugador = $("#jugador-" + nro_jugador);
+    let puntos = tablero_jugador.find(".punto");
+    let ultimo_punto = puntos.length == 0 ? null : $(puntos[puntos.length - 1]);
+    let ultimo_punto_nombre = ultimo_punto == null ? "" : ultimo_punto[0].className.match(/punto-[^\s]*/)[0];
+    let es_de_a_5 = GetPuntosXModo(modo) % 5 == 0;
+
+    let html_punto = `<div class="punto #NOMBRE_PUNTO#"></div>`;
+
+    if (ultimo_punto_nombre == "punto-arriba")
+        ultimo_punto.parent().append(html_punto.replace("#NOMBRE_PUNTO#", "punto-derecha"));
+
+    else if (ultimo_punto_nombre == "punto-derecha")
+        ultimo_punto.parent().append(html_punto.replace("#NOMBRE_PUNTO#", "punto-abajo"));
+
+    else if (ultimo_punto_nombre == "punto-abajo")
+        ultimo_punto.parent().append(html_punto.replace("#NOMBRE_PUNTO#", "punto-izquierda"));
+
+    else if (es_de_a_5 && ultimo_punto_nombre == "punto-izquierda")
+        ultimo_punto.parent().append(html_punto.replace("#NOMBRE_PUNTO#", "punto-medio"));
+
+    else if ((!es_de_a_5 && ultimo_punto_nombre == "punto-izquierda") || (ultimo_punto_nombre == "punto-medio"))
+        ultimo_punto.parent().next(".divisor-puntos").append(html_punto.replace("#NOMBRE_PUNTO#", "punto-arriba"));
+    else
+        tablero_jugador.find(".divisor-puntos:first").append(html_punto.replace("#NOMBRE_PUNTO#", "punto-arriba"));
+}
+
+function CheckearSiGanoJugador(nro_jugador) {
+    return GetDataJugador(nro_jugador).gano();
+}
+
+function RemoveTableroPuntosSiPasoABuenas(nro_jugador) {
+    if (CheckearSiPasoABuenas(nro_jugador))
+        RemoveAllTableroPuntosJugador(nro_jugador);
+}
+
+function CheckearSiPasoABuenas(nro_jugador) {
+    let puntos_x_modo = GetPuntosXModo();
+
+    if (puntos_x_modo > 15 && GetDataJugador(nro_jugador).puntos == (puntos_x_modo / 2) + 1)
+        return true;
+    else
+        return false;
+}
+
+function RemoveAllTableroPuntosJugador(nro_jugador) {
+    $("#jugador-" + nro_jugador).find(".punto").remove();
+}
+
+function RemoveAllTableroPuntos() {
+    $(".puntos").remove();
+}
+
+function DecrementarPuntoJugador(nro_jugador) {
+    let jugador = GetDataJugador(nro_jugador);
+
+    if (jugador.puntos > 0)
+        jugador.puntos -= 1;
+}
+
+function IncrementarPuntosJugador(nro_jugador) {
+    GetDataJugador(nro_jugador).puntos += 1;
+}
+
+function SetPuntosJugador(nro_jugador, puntos) {
+    GetDataJugador(nro_jugador).puntos = puntos;
+}
+
+function SetCantTableroPuntos(nro_jugador, puntos) {
+    $("#jugador-" + nro_jugador + " .cant-puntos").text(puntos);
+}
+
+function GetDataJugador(nro_jugador) {
+    return juego.jugadores[nro_jugador + ""];
+}
+
+function GetPuntosXModo(modo) {
+    return juego.puntos_x_modo[juego.modo + ""];
+}
+
 function SaveConfiguracion() {
     juego.modo = $("#modo").val();
 
     CrearPanel(1, juego.modo);
-    SetCantPuntos(1, 0);
+    SetPuntosJugador(1, 0);
     CrearPanel(2, juego.modo);
-    SetCantPuntos(2, 0);
+    SetPuntosJugador(2, 0);
 }
 
-function CrearPanel(jugador, modo) {
-    let tablero_puntos = $("#jugador-" + jugador + " .puntos");
+function CrearPanel(nro_jugador, modo) {
+    let tablero_puntos = $("#jugador-" + nro_jugador + " .puntos");
     let html_divisor = `<div class="divisor-puntos"></div>`;
 
     switch (modo) {
@@ -87,10 +162,6 @@ function CrearPanel(jugador, modo) {
             tablero_puntos.html(html_divisor + html_divisor + html_divisor + html_divisor);
             break;
     }
-}
 
-function SetCantPuntos(jugador, puntos) {
-    juego.jugadores[jugador + ""].puntos = puntos;
-    $("#jugador-" + jugador + " .cant-puntos").text(puntos);
+    SetCantTableroPuntos(nro_jugador, 0);
 }
-
